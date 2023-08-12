88ebaaf84c6839dc1957e53c8c20f19fbe9b0577
Rafael Lunardelli <rafael@devsquad.com>
2023-08-12 11:09:32 -0600

    wip

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-col gap-y-8">
-            <h1 class="text-white text-center">
+            <h1 class="dark:text-white text-center">
                 <span class="font-mono font-bold text-red-600">{{ auth()->user()->github_user }}</span>, obrigado por se registrar no sorteio do Pinguim do Laravel!!
             </h1>
         </div>
