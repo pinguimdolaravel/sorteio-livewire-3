799d45960189e847603b2a832ff589e54eaf18d3
Rafael Lunardelli <rafael@devsquad.com>
2024-01-10 17:03:14 -0700

    wip

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
-
+        User::truncate();
+        
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
