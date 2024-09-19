66ff639ff0da2186436ebc15c96350eb31fa9a36
Rafael Lunardelli <rafael@lunardelli.me>
2024-09-19 16:30:43 -0600

    wip

--- resources/views/sorteio.blade.php
+++ resources/views/sorteio.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="text-center text-3xl text-slate-300 font-bold">
-        🐧 Conference 2025 - jetete
+        🐧 mudei de novo
     </div>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-col gap-y-8">
