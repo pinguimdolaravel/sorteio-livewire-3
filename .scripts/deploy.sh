95329adc52618defcb1241bdbf13c862aab83ada
Rafael Lunardelli <rafael@lunardelli.me>
2024-09-19 16:17:42 -0600

    script de deploy

---
+++ .scripts/deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# entro no modo de manutenção
+(php artisan down) || true
+
+# pego as mudanças || true
+git pull origin production
+
+# instalar composer
+composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+
+# optimize
+php artisan optimize
+
+# compilar os assets
+# nvm
+export NVM_DIR=~/.nvm
+source ~/.nvm/nvm.sh
+
+npm run build
+
+# migrations
+php artisan migrate --force
+
+# sair do modo de manutenção
+php artisan up
