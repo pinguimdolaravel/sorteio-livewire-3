e1b829f968201c4e1ee58d0169eb4626e82d0712
Rafael Lunardelli <rafael@lunardelli.me>
2024-10-31 15:58:34 -0600

    wip

--- resources/views/blz.blade.php
+++ resources/views/blz.blade.php
@@ -1,3 +1,7 @@
 <x-guest-layout>
    Opa! Bem vindo ao sorteio. Senta e relaxa!
+
+    <x-primary-button href="{{ route('logout') }}" class="cursor-pointer">
+        Deslogar o paranauê.
+    </x-primary-button>
 </x-guest-layout>
